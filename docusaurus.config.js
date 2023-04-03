841ecca8c8d4d7fa7ad0acba41c4e11e31226d03
Sean Geelan <seanpatrick.geelan@gmail.com>
2023-04-03 16:43:11 +0200

    Logo_fix6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
       title: "NavVis VLX Documentation",
       logo: {
         alt: "Home",
-        src: "img/IVION-Logo-negative.svg",
+        src: "img/VLX4.svg",
       },
      
     },
