5a1d77de2d42e29ff022826e5b557da81a04dfcd
Sean Geelan <seanpatrick.geelan@gmail.com>
2023-05-03 13:05:01 +0200

    UserGuide

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
       title: "NavVis IVION",
       logo: {
         alt: "NavVis VLX Documentation",
-        src: "img/NavVis_Logo_Digital_White_vlx2.png",
+        src: "img/NavVis_Logo_Digital_White_vlx3.png",
       },
      
     },
